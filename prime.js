a28011d29c3aeabd95e8cffe6de56c4802aa28e4
abhi-upadhyay2109 <panditabhishek9651@gmail.com>
2023-08-27 20:41:47 +0530

    check prime no.

---
+++ prime.js
@@ -0,0 +1,20 @@
+let number =13;
+
+let count=0;
+
+for(let i=1; i<=number; i++)
+{
+    if(number  % i==0)
+    {
+      count++
+    }
+}
+
+if(count==5)
+{
+    console.log("prime")
+}
+else
+{
+    console.log("Not prime")
+}
